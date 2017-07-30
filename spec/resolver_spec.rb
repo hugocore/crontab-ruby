1fba73e964c824443f89100d2851c02f6f7236b7
Hugo Sequeira <hugoandresequeira@gmail.com>
2017-07-30 21:24:24 +0100

    Use range in spec instead of array

--- spec/resolver_spec.rb
+++ spec/resolver_spec.rb
@@ -22,8 +22,8 @@ RSpec.describe Resolver do
         min: [0, 15, 30, 45],
         hour: [0],
         day: [1, 15],
-        month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        day_week: [1, 2, 3, 4, 5],
+        month: (1..12).to_a,
+        day_week: (1..5).to_a,
         cmd: '/usr/bin/find'
       }
 
